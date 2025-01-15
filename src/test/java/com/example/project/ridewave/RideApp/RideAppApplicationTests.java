e810c65eb8512de495133eb99b7f6fd463b79bef
Sarvpreet Singh <luffytaaro112@gmail.com>
2025-01-15 11:03:21 +0530

    Adding Initial Files

---
+++ src/test/java/com/example/project/ridewave/RideApp/RideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.ridewave.RideApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
